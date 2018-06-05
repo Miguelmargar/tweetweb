038862522135002f11799546718b9b686be693cb
Xavier <xavier.astor.fontaine@gmail.com>
2018-06-05 14:10:59 +0000

    Search function

--- tweets.py
+++ tweets.py
@@ -7,12 +7,12 @@ api = get_api()
 def search(query, count):
     return [status for status in tweepy.Cursor(api.search, q=query).items(count)]
 
-topic = input("Enter topic: " )
-numbertweets = int(input("Enter number of tweets: "))
+# topic = input("Enter topic: " )
+# numbertweets = int(input("Enter number of tweets: "))
     
-tweets = search(topic, numbertweets)
+# tweets = search(topic, numbertweets)
 
-for tweet in tweets:
-    print(tweet.text)
+# for tweet in tweets:
+#     print(tweet.text)
 
 
--- tweetweb.py
+++ tweetweb.py
@@ -1,16 +1,19 @@
 from flask import *
 import os
+from tweets import search
 
 app = Flask(__name__)
 
-
 @app.route("/")
-def hello_world():
+def get_index():
     return render_template("index.html")
 
 @app.route("/results")
 def results():
-    return render_template("results.html")
+    search  = request.form['search'] 
+    number  = request.form['number'] 
+    tweets  = search(search,number)
+    return render_template("results.html", tweets = tweets )
 
 if __name__ == '__main__':
     app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug=True)
