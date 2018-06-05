b6a73bd5cf49da314733e40bd5acdea602ccd64b
Miguel Martinez <miguelitalia@hotmail.com>
2018-06-05 11:03:59 +0000

    tweet search and prints functinality added

--- tweets.py
+++ tweets.py
@@ -12,11 +12,7 @@ numbertweets = int(input("Enter number of tweets: "))
     
 tweets = search(topic, numbertweets)
 
-print("Text of tweet: " + tweets[0].text)
-print("User's screen name: " + tweets[0].user.screen_name)
-hashtags = [h["text"] for h in tweets[0].entities["hashtags"]]
-print("Hashtags: " + str(hashtags))
-print("User's followers: " + str(tweets[0].user.followers_count))
-print("User's friends: " + str(tweets[0].user.friends_count))
-print("User's Tweet Count: " + str(tweets[0].user.statuses_count))
+for tweet in tweets:
+    print(tweet.text)
+
 
